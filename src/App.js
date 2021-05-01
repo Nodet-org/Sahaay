da8f179c010ada08b74f70becfeb05442bdee2fe
Abhinav Rajesh <abhinavrajesh49@gmail.com>
2021-05-01 23:03:49 +0530

    Add resource form kinda done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AddResource from "./components/AddResource";
 
 function App() {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative min-h-screen home__container">
       <Header />
       <AddResource />
       <div className="App">COVID19 Resources</div>
--- src/components/AddResource/index.js
+++ src/components/AddResource/index.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { Modal, Form, Input } from "antd";
+import { Modal, Form, Input, Select } from "antd";
 
 import plusIcon from "../../assets/plus.svg";
 
-import "antd/dist/antd.css";
+const { Option } = Select;
 
 const AddResource = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -12,8 +12,8 @@ const AddResource = () => {
     setIsModalVisible(true);
   };
 
-  const handleOk = () => {
-    setIsModalVisible(false);
+  const handleSubmit = (value) => {
+    console.log(value);
   };
 
   const handleCancel = () => {
@@ -28,17 +28,94 @@ const AddResource = () => {
       <Modal
         title="Add Resource"
         visible={isModalVisible}
-        onOk={handleOk}
         onCancel={handleCancel}
         centered
+        footer={false}
       >
-        <Form layout="vertical">
-          <Form.Item label="Name" requiredMark={false} name="name">
+        <Form
+          layout="vertical"
+          onFinish={handleSubmit}
+          initialValue={{
+            resource: "oxygen",
+          }}
+        >
+          <Form.Item
+            label="Resource"
+            requiredMark={false}
+            name="resource"
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: "Please select a resource",
+              },
+            ]}
+          >
+            <Select>
+              <Option value="oxygen">Oxygen Cylinder</Option>
+              <Option value="bed">Beds</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            requiredMark={false}
+            name="name"
+            tooltip="Enter your name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Phone" requiredMark={false} name="phone">
+          <Form.Item
+            label="Phone"
+            requiredMark={false}
+            name="phone"
+            tooltip="Enter your phone number"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone",
+              },
+            ]}
+          >
             <Input />
           </Form.Item>
+          <Form.Item
+            label={[
+              "Email",
+              <span className="text-gray-400 pl-1">(Optional)</span>,
+            ]}
+            requiredMark={false}
+            name="email"
+            tooltip="Enter your email (Optional)"
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Price"
+            requiredMark={false}
+            name="price"
+            tooltip="Enter the price. Enter 0 if for free"
+            rules={[
+              {
+                required: true,
+                message: "Please input your price. If free enter 0",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Input type="submit" />
         </Form>
       </Modal>
     </>
